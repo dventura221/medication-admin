47465f07a52fa7f5ccaa0ea20ba464fd4b6f1613
Dan Ventura <danice.ventura@gmail.com>
2022-03-08 09:52:28 -0700

    added eventlisters to 2 buttons, added function to add to sessionStorage when clicked, success with storing data, need to figure out how to pull data down on other html page

--- taco.js
+++ taco.js
@@ -1,1 +1,49 @@
-console.log('Hello')
+//Global Variables
+let data = sessionStorage.setItem
+const buttonLis = document.getElementById('buttonLis')
+const buttonAsp = document.getElementById('buttonAsp')
+
+//Global Functions
+
+// function storageAvailable(type) {
+//   var storage
+//   try {
+//     storage = window[type]
+//     var x = '__storage_test__'
+//     storage.setItem(x, x)
+//     storage.removeItem(x)
+//     return true
+//   } catch (e) {
+//     return (
+//       e instanceof DOMException &&
+//       // everything except Firefox
+//       (e.code === 22 ||
+//         // Firefox
+//         e.code === 1014 ||
+//         // test name field too, because code might not be present
+//         // everything except Firefox
+//         e.name === 'QuotaExceededError' ||
+//         // Firefox
+//         e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+//       // acknowledge QuotaExceededError only if there's something already stored
+//       storage &&
+//       storage.length !== 0
+//     )
+//   }
+// }
+// if (storageAvailable('localStorage')) {
+//   console.log('Yippee! We can use localStorage awesomeness')
+// } else {
+//   console.log('Too bad, no localStorage for us')
+// }
+
+//Event Listeners
+buttonLis.addEventListener('click', () => {
+  sessionStorage.setItem('Lis', 'yes')
+  console.log(sessionStorage)
+})
+
+buttonAsp.addEventListener('click', () => {
+  sessionStorage.setItem('Asp', 'yes')
+  console.log(sessionStorage)
+})
