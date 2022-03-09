891083a6384db0bb2dcb6f5003c5e0d9b9e2f412
Dan Ventura <danice.ventura@gmail.com>
2022-03-08 21:48:31 -0700

    added event listeners to all the medRoom buttons

--- taco.js
+++ taco.js
@@ -2,6 +2,10 @@
 //let data = sessionStorage.getItem('Lis')
 const buttonLis = document.getElementById('buttonLis')
 const buttonAsp = document.getElementById('buttonAsp')
+const buttonEsci = document.getElementById('buttonEsci')
+const buttonTraz = document.getElementById('buttonTraz')
+const buttonOmep = document.getElementById('buttonOmep')
+const buttonGaba = document.getElementById('buttonGaba')
 const ptRoomCheck = document.getElementById('PagePatientRoom')
 const buttonMedReturn = document.getElementById('return-meds')
 const heldMeds = document.getElementById('All-Held-Meds')
@@ -17,7 +21,7 @@ let medCarryOver = () => {
     //console.log(sessionStorage.getItem(key)) -- string of value
     const newButton = document.createElement('button')
     newButton.innerText = sessionStorage.getItem(key)
-    newButton.classList.add('ptRoom-button')
+    newButton.classList.add('ptRoom-med-button')
     heldMeds.appendChild(newButton)
   }
 }
@@ -48,5 +52,25 @@ buttonLis.addEventListener('click', () => {
 
 buttonAsp.addEventListener('click', () => {
   sessionStorage.setItem('Asp', 'Aspirin 81 mg tablet')
+  console.log(sessionStorage)
+})
+
+buttonEsci.addEventListener('click', () => {
+  sessionStorage.setItem('Esci', 'Escitalopram 10 mg tablet')
+  console.log(sessionStorage)
+})
+
+buttonTraz.addEventListener('click', () => {
+  sessionStorage.setItem('Traz', 'Trazodone 50 mg tablet')
+  console.log(sessionStorage)
+})
+
+buttonOmep.addEventListener('click', () => {
+  sessionStorage.setItem('Omep', 'Omeprazole 20 mg capsule')
+  console.log(sessionStorage)
+})
+
+buttonGaba.addEventListener('click', () => {
+  sessionStorage.setItem('Gaba', 'Gabapentin 300 mg capsule')
   console.log(sessionStorage)
 })
